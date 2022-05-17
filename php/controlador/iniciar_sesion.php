138189d9ed0d9f2cf13254e5983f539442caa1ac
Manel <82091949+ManelFix@users.noreply.github.com>
2022-05-17 12:54:55 +0200

    Add files via upload

--- php/controlador/iniciar_sesion.php
+++ php/controlador/iniciar_sesion.php
@@ -7,11 +7,9 @@ header("Content-Type: application/xml");
 
 $word = explode(",",$_POST["word"]);
 
-$passwrd = md5($word[1]);
-$user = md5($word[0]);
+// $passwrd = md5($word[1]);
 
-
-$sql="SELECT COUNT(*) as contador FROM usuari WHERE nom_usuari = '$user' AND contrasenya = '$passwrd'";
+$sql="SELECT COUNT(*) as contador FROM usuari WHERE nom_usuari = '$word[0]' AND contrasenya = '$word[1]'";
 
 $res=mysqli_query($connexio, $sql);
 
@@ -20,7 +18,7 @@ $fila = mysqli_fetch_assoc($res);
 if($fila["contador"]==0){
 echo 0;
 }else if($fila["contador"]==1){
-    $sql="SELECT id_usuari,ban,premium admin  FROM usuari WHERE nom_usuari = '$user' AND contrasenya = '$passwrd'";
+    $sql="SELECT id_usuari, admin  FROM usuari WHERE nom_usuari = '$word[0]' AND contrasenya = '$word[1]'";
 
     $res=mysqli_query($connexio, $sql);
 
@@ -33,9 +31,6 @@ echo 0;
         echo 2;
     }else{
         $_SESSION["ses_id"]=$fila["id_usuari"];
-        $_SESSION["ban"]=$fila["ban"];
-        $_SESSION["premium"]=$fila["premium"];
-
         echo 1;
     }
 }
